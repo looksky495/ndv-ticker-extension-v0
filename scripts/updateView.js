c39692adb3542e0d314420f196e4cbdaad2d5c56
looksky495 (Mac) <tuyatuya_muhco@icloud.com>
2026-05-31 13:45:50 +0900

    updateNotice で markdown 使えるように

--- scripts/updateView.js
+++ scripts/updateView.js
@@ -1,5 +1,17 @@
-let sy = window.location.search.substring(1).split('&').map((p) => p.split('=')).reduce((obj, e) => ({...obj, [e[0]]: e[1]}), {});
-document.getElementById("current").innerText = decodeURIComponent(sy.app);
-document.getElementById("latest").innerText = decodeURIComponent(sy.new);
-document.querySelector("textarea").value = "今回のアップデート内容：\n"+decodeURIComponent(sy.txt);
-document.querySelector("a").href = decodeURIComponent(sy.url);
+const urlParams = new URLSearchParams(window.location.search);
+const obj = Object.fromEntries(urlParams.entries());
+
+const content = decodeURIComponent(obj.txt);
+
+document.getElementById("current").innerText = decodeURIComponent(obj.app);
+document.getElementById("latest").innerText = decodeURIComponent(obj.new);
+if (navigator.userAgentData.brands[0].brand === "Chromium" && navigator.userAgentData.brands[0].version >= 146){
+  document.getElementById("content").setHTML(marked.parse(content));
+} else if (Element.prototype.setHTML){
+  document.getElementById("content").setHTML(marked.parse(content));
+} else {
+  document.getElementById("content").innerText = content;
+  document.getElementById("content").style.whiteSpace = "pre-wrap";
+  document.getElementById("content").style.fontFamily = "monospace";
+}
+document.querySelector("a").href = decodeURIComponent(obj.url);
