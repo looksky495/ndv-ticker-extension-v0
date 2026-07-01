812de47daff76f313e8f9a78347b3c1f4cb30eea
looksky495 (Mac) <tuyatuya_muhco@icloud.com>
2026-07-02 01:23:15 +0900

    beta 0.7.4

--- scripts/main.js
+++ scripts/main.js
@@ -47,10 +47,11 @@ const AppVersionHistory = [
   "β0.7.1",
   "β0.7.2",
   "β0.7.3",
+  "β0.7.4",
 ];
 
-const AppVersionCode = "beta35";
-const AppVersionView = "β0.7.3";
+const AppVersionCode = "beta36";
+const AppVersionView = "β0.7.4";
 console.log(`%cNDV %c(Natural Disaster Viewer)%c   v.${AppVersionView}%c`,
   "background: #9f9; font-family: sans-serif; font-weight: 700; padding: 2px; font-size: 19px; font-style: italic;",
   "background: #9f9; font-family: sans-serif; font-weight: 700; padding: 2px; font-size: 11px; font-style: italic;",
